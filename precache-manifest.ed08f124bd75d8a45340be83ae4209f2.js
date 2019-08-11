ff7785a179bedb6263bc58f02c39af9ec554a841
datdev <datdev0609@gmail.com>
2019-08-11 17:25:48 +0700

    Updates

---
+++ precache-manifest.ed08f124bd75d8a45340be83ae4209f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d043ece385c8022793e190df7e706d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "79e31442ecc719973957",
+    "url": "/my-app/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "bdf318e90a08eec2549a",
+    "url": "/my-app/static/js/2.a8d6398c.chunk.js"
+  },
+  {
+    "revision": "79e31442ecc719973957",
+    "url": "/my-app/static/js/main.fae1a090.chunk.js"
+  },
+  {
+    "revision": "a1d6f7139752f47f6a5e",
+    "url": "/my-app/static/js/runtime~main.fe2b4f92.js"
+  }
+]);
